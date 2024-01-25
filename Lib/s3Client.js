04fc80e8fb4fb1a39d51bd797fdc5acb4e1ceb3c
YusakuTanakaKM <yusaku.tanaka@konicaminolta.com>
2024-01-25 17:42:51 +0900

    ap-northeast-1 -> eu-west-2

--- Lib/s3Client.js
+++ Lib/s3Client.js
@@ -1,7 +1,7 @@
 
 import { s3Client } from "@aws-sdk/client-s3";
 // Set the AWS Region.
-const REGION = "ap-northeast-1"; //e.g. "us-east-1"
+const REGION = "eu-west-2"; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
 const s3Client = new s3ClientClient({ region: REGION });
 export { s3Client };
