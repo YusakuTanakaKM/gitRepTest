c52bdeed553fb0e0bc53ec1908ebf33aa0370d22
YusakuTanakaKM <yusaku.tanaka@konicaminolta.com>
2024-01-25 19:33:34 +0900

    credentials added

--- Lib/s3Client.js
+++ Lib/s3Client.js
@@ -3,5 +3,8 @@ import { s3Client } from "@aws-sdk/client-s3";
 // Set the AWS Region.
 const REGION = "eu-west-2"; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
-const s3Client = new s3ClientClient({ region: REGION });
+const s3Client = new s3ClientClient({
+     region: REGION,
+     credentials: { accessKeyId: 'AKIAR3ZPSJ4WCV5X4HGY', secretAccessKey: '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' }, 
+});
 export { s3Client };
